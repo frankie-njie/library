529cb96b1585fa1a5764af6ba5ac1263dddab07a
frankie-njie <njie_frankie@yahoo.com>
2020-10-06 21:25:57 +0100

    some changes

--- scripts.js
+++ scripts.js
@@ -1,16 +1,31 @@
 let myLibrary = [];
 
-// function addBookToLibrary() {}
-
 //book constructor
-function Book(title, author, pages, read) {
+function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read
-    this.info = function() {
-        return `${title} by ${author} , ${pages} , ${read}`;
-    }
+    this.status = status;
 }
-const bookName = new Book('Book Name Example ', 'John book', '200', 'read alredy');
-console.log(bookName.info());
+
+Book.prototype.info = function(){
+    return `${this.title} by ${this.author} , has ${this.pages} pages  , ${this.status}`;
+}
+
+const firstBook = new Book('Book Name Example ', 'John book', '200', 'read already');
+
+let insert = document.querySelector(".add-book");
+insert.addEventListener("click", addBookToLibrary(titleOfBook, authorOfBook, pagesOfBook, statusOfBook));
+//add new book function
+function addBookToLibrary(titleOfBook, authorOfBook, pagesOfBook, statusOfBook) {
+    titleOfBook = document.querySelector(".title-input").value;
+    authorOfBook = document.querySelector(".author-input").value;
+    pagesOfBook = document.querySelector(".pages-input").value;
+    statusOfBook = document.querySelector(".status-input").value;
+
+    let newBook = new Book(titleOfBook, authorOfBook, pagesOfBook, statusOfBook);
+    console.log(newBook.info());
+
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+}
